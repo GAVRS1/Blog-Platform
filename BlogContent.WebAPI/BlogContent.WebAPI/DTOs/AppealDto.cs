a786827a1d4c9fb233646abf9a7df5f66e51d694
0xgavrs <119516413+GAVRS1@users.noreply.github.com>
2026-02-01 17:01:56 +0300

    Add usernames to moderation DTOs

--- BlogContent.WebAPI/BlogContent.WebAPI/DTOs/AppealDto.cs
+++ BlogContent.WebAPI/BlogContent.WebAPI/DTOs/AppealDto.cs
@@ -7,6 +7,7 @@ public class AppealDto
     public int Id { get; set; }
     public int ModerationActionId { get; set; }
     public int UserId { get; set; }
+    public string? Username { get; set; }
     public string Message { get; set; } = string.Empty;
     public string? Resolution { get; set; }
     public AppealStatus Status { get; set; }
--- BlogContent.WebAPI/BlogContent.WebAPI/DTOs/ReportDto.cs
+++ BlogContent.WebAPI/BlogContent.WebAPI/DTOs/ReportDto.cs
@@ -6,7 +6,9 @@ public class ReportDto
 {
     public int Id { get; set; }
     public int ReporterUserId { get; set; }
+    public string? ReporterUsername { get; set; }
     public int? TargetUserId { get; set; }
+    public string? TargetUsername { get; set; }
     public int? PostId { get; set; }
     public int? CommentId { get; set; }
     public string Reason { get; set; } = string.Empty;
